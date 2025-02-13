65976d2a05d91c98de01853120998f1d063d46b0
Evan <evanne.scherrer@gmail.com>
2025-02-13 11:16:00 -0600

    add player and stuff

---
+++ players/player.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+enum Direction {UP, RIGHT, DOWN, LEFT}
+
+var tile_position: Vector2i = Vector2i(0, 0)
+var TILE_SIZE: int = 16
+
+func _ready() -> void:
+	self.position = tile_position * TILE_SIZE + Vector2i(8, 8)
+	
+	#while true:
+		#await get_tree().create_timer(1).timeout
+		#self.make_action()
+
+func move(direction: Direction):
+	match direction:
+		Direction.UP:
+			self.position += Vector2(0, -1) * TILE_SIZE
+		Direction.RIGHT:
+			self.position += Vector2(1, 0) * TILE_SIZE
+		Direction.LEFT:
+			self.position += Vector2(-1, 0) * TILE_SIZE
+		Direction.DOWN:
+			self.position += Vector2(0, 1) * TILE_SIZE
+
+func make_action():
+	self.move(Direction.values()[randi() % Direction.size()])
+
+func _process(delta: float) -> void:
+	pass
---
+++ turn-controller.gd
@@ -0,0 +1,26 @@
+extends Node2D
+@onready var tilemap = $TileMapLayer
+
+@onready var player1 = $Player
+var players: Array = []
+var enemies: Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().process_frame
+	players.append(player1)
+
+	while true:
+		for player in players:
+			print(player)
+			player.make_action()
+			await get_tree().create_timer(1.5).timeout
+		
+		for enemy in enemies:
+			enemy.get_move()
+			await get_tree().create_timer(1).timeout
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
