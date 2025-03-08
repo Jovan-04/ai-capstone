537adc17932920df0e88517d1f5325ffca520868
Nicholas Haisler <116696773+NHaisler@users.noreply.github.com>
2025-03-08 17:43:03 -0600

    Added Main Menu Selection Screen
    You can now toggle on or off AI with checkboxes on start. Although the values don't do anything as of now, they are just on the main menu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -2,6 +2,17 @@ extends Control
 
 @onready var play_button: Button = $PlayButton/Button
 @onready var quit_button: Button = $Quit/Button
+@onready var check_box_assassin: CheckBox = $Selection/CheckBoxAssassin
+@onready var check_box_paladin: CheckBox = $Selection/CheckBoxPaladin
+@onready var check_box_wizard: CheckBox = $Selection/CheckBoxWizard
+var played_by_real_player_assassin = false
+var played_by_real_player_paladin = false
+var played_by_real_player_wizard = false
+
+
+
+
+
 const GAME = preload("res://scenes/game.tscn")
 
 func _ready() -> void:
@@ -13,3 +24,21 @@ func _on_play_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_check_box_assassin_toggled(toggled_on: bool) -> void:
+	played_by_real_player_assassin = toggled_on
+	print(played_by_real_player_assassin)
+
+
+func _on_check_box_paladin_toggled(toggled_on: bool) -> void:
+	played_by_real_player_paladin = toggled_on
+	print(played_by_real_player_paladin)
+	
+
+
+func _on_check_box_wizard_toggled(toggled_on: bool) -> void:
+	played_by_real_player_wizard = toggled_on
+	print(played_by_real_player_wizard)
+	
+	
